7202302e001886832cd1aee7f17862e43bcd479c
Jonathan Costa <111503993+JhonCostaDev@users.noreply.github.com>
2025-05-21 19:20:55 +0000

    refactor: increasing knowledge about dataframes

--- chapter_5_starting_pandas/dataframes.py
+++ chapter_5_starting_pandas/dataframes.py
@@ -16,3 +16,74 @@ data = {
 
 df = pd.DataFrame(data)
 divider(df)
+
+# Selecting only the first five rows:
+divider(df.head())
+
+# Specifying the sequence of columns:
+
+divider(pd.DataFrame(data, columns=['year', 'states', 'pop']))
+
+# If we pass a column that isn't contained in the dict, it will appear with missing values in the result:
+
+df2 = pd.DataFrame(data, columns=['year', 'states', 'pop', 'debt'], index=['one', 'two', 'three', 'four', 'five', 'six'])
+
+divider(df2)
+
+# A column in a DataFrame can be retrieved as a Series either by dict-like notation or by attribute:
+
+divider(df2['states'])
+divider(df2.year)
+
+'''
+Note that the returned Series have the same index as the DataFrame, and theri name attribute has been appropriately set.
+
+Rows can also be retrieved by position or name with the special loc attribute (much more on this later):
+'''
+
+divider(df2.loc['three']) # by value
+
+# Retrive information usaing the Iloc
+
+divider(df2.iloc[0]) # By index
+
+# Modifying Columns
+
+# Columns can be modified by assignment.
+# For example, the empty 'debt' column could be assigned a scalar value or an array of values:
+
+df2['debt'] = 16.2
+
+divider(df2)
+
+df2['debt'] = np.arange(6.)
+
+divider(df2)
+
+'''
+When you are assigning lists or arrays to a column, the value's length must match the length of the DataFrame. If you assign a Series, its labels will be realigned exactly to the DataFrame's index, inserting missing values in any holes: 
+'''
+
+val = pd.Series([-1.2, -1.5, -1.7], index=['two', 'four', 'five'])
+df2['debt'] = val
+
+divider(df2)
+
+# CREATE A NEW COLUMN
+
+'''
+Assigning a column that doesn't exist will create a new column. The del keyword will delete columns as with a dict.
+
+As an exemple of del, I first add a new column of boolean values where the states columns equals 'Ohio':
+'''
+
+df2['Eastern'] = df2.states == 'Ohio'
+
+divider(df2)
+
+
+#Removing a column with del:
+
+del df2['Eastern']
+
+divider(df2)
